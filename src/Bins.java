0909ab0ffacca7076126cccfccb3f62e708934f7
Harry Guo <flashropower@gmail.com>
2016-01-14 17:34:05 -0500

    changes to code

--- src/Bins.java
+++ src/Bins.java
@@ -27,14 +27,41 @@ public class Bins {
     }
     
     /**
-     * Method to print all the information
-     * 
+     * We just need a method that takes in the heuristic 
      */
     
-    public static void binSort(String heuristic){
+    public static void binSort(List<Integer> data, String heuristic){
     	if (heuristic == "decreasing"){
     		Collections.sort(data, Collections.reverseOrder());
     	}
+    	
+    	 PriorityQueue<Disk> pq = new PriorityQueue<Disk>();
+         pq.add(new Disk(0));
+
+         int diskId = 1;
+         
+         for (Integer size : data) {
+             Disk d = pq.peek();
+             if (d.freeSpace() >= size) {
+                 pq.poll();
+                 d.add(size);
+                 pq.add(d);
+             } else {
+                 Disk d2 = new Disk(diskId);
+                 diskId++;
+                 d2.add(size);
+                 pq.add(d2);
+             }
+         }
+  
+         System.out.println();
+         System.out.println(heuristic);
+         System.out.println("number of pq used: " + pq.size());
+         while (!pq.isEmpty()) {
+             System.out.println(pq.poll());
+         }
+         System.out.println();
+    	
     }
 
     /**
@@ -44,61 +71,14 @@ public class Bins {
         Bins b = new Bins();
         Scanner input = new Scanner(Bins.class.getClassLoader().getResourceAsStream(DATA_FILE));
         List<Integer> data = b.readData(input);
-
-        PriorityQueue<Disk> pq = new PriorityQueue<Disk>();
-        pq.add(new Disk(0));
-
-        int diskId = 1;
+        
         int total = 0;
-        for (Integer size : data) {
-            Disk d = pq.peek();
-            if (d.freeSpace() > size) {
-                pq.poll();
-                d.add(size);
-                pq.add(d);
-            } else {
-                Disk d2 = new Disk(diskId);
-                diskId++;
-                d2.add(size);
-                pq.add(d2);
-            }
-            total += size;
+        for (Integer size: data){
+        	total += size;
         }
-
         System.out.println("total size = " + total / 1000000.0 + "GB");
-        System.out.println();
-        System.out.println("worst-fit method");
-        System.out.println("number of pq used: " + pq.size());
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
-        System.out.println();
+        binSort(data, "worst fit");
+        binSort(data, "decreasing");
 
-        Collections.sort(data, Collections.reverseOrder());
-        pq.add(new Disk(0));
-
-        diskId = 1;
-        for (Integer size : data) {
-            Disk d = pq.peek();
-            if (d.freeSpace() >= size) {
-                pq.poll();
-                d.add(size);
-                pq.add(d);
-            } else {
-                Disk d2 = new Disk(diskId);
-                diskId++;
-                d2.add(size);
-                pq.add(d2);
-            }
-     
-
-        System.out.println();
-        System.out.println("worst-fit decreasing method");
-        System.out.println("number of pq used: " + pq.size());
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
-        System.out.println();
-    }
     }
 }
